6d09a6013c21aa7174a9b5bca4a8c077b7865f46
Bruno De Toffol <msn-bruno@hotmail.com>
2025-03-24 12:11:35 -0300

    Resolvido exercicio 20

---
+++ Exercicio20/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Escrever um algoritmo que leia um valor para uma variável N de 1 a 10 e calcule a tabuada de N. Mostre a tabuada na forma:
+                ● 0 x N = 0,
+                ● 1 x N = 1N,
+                ● 2 x N = 2N,
+                        .
+                ● 10 x N = 10N. */
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Digite um valor pra N: ");
+            int varN = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("----------------------------");
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{i} x {varN} = {i}N");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
