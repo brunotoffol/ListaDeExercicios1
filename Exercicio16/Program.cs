ec73db0f2d6d74898fd703f0229749555e080038
Bruno De Toffol <msn-bruno@hotmail.com>
2025-03-24 11:27:09 -0300

    Resolvido Exercicio 16

---
+++ Exercicio16/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Informe um valor: ");
+            double valor = Convert.ToDouble(Console.ReadLine());
+
+            if (valor % 2 == 0)
+
+            {
+                Console.WriteLine("---------------------------");
+                Console.WriteLine($"O valor informado é par.");
+            }
+            else
+            {
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("O valor informado é impar.");
+            }
+
+            Console.ReadLine();
+        }        
+    }
+}
