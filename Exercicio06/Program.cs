90782fe7ab175db78a77f244cb7d47bd767e0134
Bruno De Toffol <msn-bruno@hotmail.com>
2025-03-24 01:13:49 -0300

    Resolvido Exercicio 06

---
+++ Exercicio06/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercicio06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Crie um programa para calcular a média harmônica das notas de um Aluno
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Digite suas notas: ");
+            string[] notasInformadas = Console.ReadLine().Split(' ');
+            double[] notasConvertida = new double[notasInformadas.Length];
+
+            
+            for (int i = 0; i < notasInformadas.Length; i++)
+            {
+                notasConvertida[i] = double.Parse(notasInformadas[i]);
+            }
+
+            
+            double somaNotas = 0;
+            for (int i = 0; i < notasInformadas.Length; i++)
+            {
+                somaNotas += 1.0 / notasConvertida[i];
+            }
+
+            double mediaNotas = notasConvertida.Length / somaNotas;
+            Console.WriteLine($"A média harmônica é: {mediaNotas}");
+            Console.ReadLine();
+        }
+    }
+}
+
