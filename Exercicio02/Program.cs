b392fbad07c777f17c78265018ddcdc37f67f649
Bruno De Toffol <msn-bruno@hotmail.com>
2025-03-23 23:47:11 -0300

    Resolvido exercício 02

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crie um programa para calcular o volume de um Cilindro
+
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Insira o valor do raio: ");
+            double raio = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Insira o valor da altura: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("-------------------------------");
+
+            double volume = Math.PI * Math.Pow(raio, 2) * altura;
+            Console.WriteLine($"O volume do cilindro é {volume}");
+
+            Console.ReadLine();
+        }
+    }
+}
