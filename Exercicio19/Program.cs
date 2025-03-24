d855257821a80a9001738f218601ed05e0af7521
Bruno De Toffol <msn-bruno@hotmail.com>
2025-03-24 11:54:41 -0300

    Resolvido Exercicio 19

---
+++ Exercicio19/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Desenvolver um algoritmo que efetue a soma de todos os números ímpares que são múltiplos de três e que se encontram no conjunto dos números de 1 até 500.
+
+            int soma = 0;
+            int numero;
+
+            for (numero = 1; numero <= 500; numero++)
+            {
+                if (numero % 2 != 0 && numero % 3 == 0)
+                {
+                    soma += numero;
+                }
+            }
+            Console.WriteLine($"A soma de todos os números ímpares que são múltiplos de três entre 1 a 500 é: {soma}");
+            Console.ReadLine();
+        }
+    }
+}
