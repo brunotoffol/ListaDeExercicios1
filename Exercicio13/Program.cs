19ae8d1d5b24e7087b06eb5a5fef37fd0ba9fcde
Bruno De Toffol <msn-bruno@hotmail.com>
2025-03-24 10:11:52 -0300

    Resolvido Exercicio 13

---
+++ Exercicio13/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exercicio13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Informe o valor de A: ");
+            double valorA = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe o valor de B: ");
+            double valorB = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe o valor de C: ");
+            double valorC = Convert.ToDouble(Console.ReadLine());
+
+            double somaAB = valorA + valorB;
+
+            if (somaAB < valorC)
+            {
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("A soma de A+B é menor que C");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("A soma de A+B é maior que C");
+                Console.ReadLine();
+            }
+
+
+            
+        }
+    }
+}
