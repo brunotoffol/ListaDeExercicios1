1a5e911dc9886239ee1b5e4c4eade0eb5639e680
Bruno De Toffol <msn-bruno@hotmail.com>
2025-03-24 00:25:03 -0300

    Resolvido Exercicio 04

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercicio04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Insira a temperatura em Celsius:");
+            double tempCelsius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("----------------------------------");
+            double tempFahrenheit = (tempCelsius * 1.8) + 32;
+            Console.WriteLine($"A temperatura em Fahrenheit é: {tempFahrenheit}ºF");
+            Console.ReadLine(); 
+        }
+    }
+}
