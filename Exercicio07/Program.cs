d1f79987378b7ef3352117c498cbe57b64217405
Bruno De Toffol <msn-bruno@hotmail.com>
2025-03-24 08:49:47 -0300

    Resolvido exercicio 7

---
+++ Exercicio07/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercicio07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crie um programa para calcular a média ponderada de duas provas realizadas por um aluno
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Informe a quantidade de notas que deseja fazer a média: ");
+            int numeroDeNotas = int.Parse(Console.ReadLine());
+
+            double somaDosPesos = 0;
+            double somaNotas = 0;
+
+            for (int i = 0; i < numeroDeNotas; i++)
+            {
+                Console.WriteLine($"Digite sua nota: ");
+                double valorNota = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o peso para a nota informada: ");
+                double pesoNota = int.Parse(Console.ReadLine());
+
+                somaNotas += valorNota * pesoNota;
+                somaDosPesos += pesoNota;
+            }
+
+            double mediaNotas = somaNotas / somaDosPesos;
+            Console.WriteLine($"A média ponderada para as suas notas é: {mediaNotas}");
+
+            Console.ReadLine();
+        }
+    }
+}
