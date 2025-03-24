a9ff03859a1d5594195beb42b8b0ae100a152792
Bruno De Toffol <msn-bruno@hotmail.com>
2025-03-24 09:33:32 -0300

    Resolvido Exercicio 09

---
+++ Exercicio09/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //A imobiliária Imóbilis vende apenas terrenos retangulares. Faça um algoritmo para ler as dimensões de um terreno e depois exibir a área do terreno.
+
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Informe o comprimento do terreno: ");
+            int comprimentoTerreno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe a largura do terreno: ");
+            int larguraTerreno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("--------------------------------------");
+
+            int areaTerreno = comprimentoTerreno * larguraTerreno;
+            Console.WriteLine($"A área do terreno é {areaTerreno} m².");
+            Console.ReadLine();
+        }
+    }
+}
